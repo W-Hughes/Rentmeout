6e9f82419af07ee033c49ea0c2e02f6861dc0d42
W Hughes <snypgeee@gmail.com>
2025-04-11 22:46:43 +0000

    Designed a backdrom for user sign modal

---
+++ src/components/shared/Backdrop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="model__backdrop">
+    <div class="backdrop">
+      <slot>
+        <p>--Default slot--</p>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackdropComponent",
+};
+</script>
+
+<style>
+.backdrop {
+  @apply absolute top-0 left-0 bg-black bg-opacity-20 h-screen w-full flex justify-center pt-[10rem];
+}
+</style>
